0b18b83a442170836a37dd71c7f2fd90b50733d8
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-12 15:12:07 +0300

    up

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,33 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", createGallery(galleryItems));
+gallery.addEventListener("click", onClick);
+
+function createGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                 <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+              </li> `;
+    })
+    .join("");
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+  galleryLightbox.open();
+}
+
+function createModalLiteBox() {
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+  return lightbox;
+}
+const galleryLightbox = createModalLiteBox();
